95b55b87205f8d513158501900670f5a26c30547
TDS <tds@tcg3pl.com>
2024-03-27 09:00:45 -0400

    Copied from MS Copilot.

--- wcn_st_app.py
+++ wcn_st_app.py
@@ -7,7 +7,7 @@ from io import BytesIO
 def main():
     st.set_page_config(layout="wide")
     st.title("Filter Invoices by pre-defined date range and product list (WCN)")
-    st.subheader('File must be in same format as _Invoice Report All Jan23-Mar24.xlsx_')
+    st.subheader('File must be in the same format as _Invoice Report All Jan23-Mar24.xlsx_')
 
     # File upload section
     uploaded_file = st.file_uploader("Upload an Excel file", type=["xlsx"])
@@ -18,20 +18,15 @@ def main():
             # Read the Excel file
             df = pd.read_excel(uploaded_file, skiprows=4, engine='openpyxl')
             df = df.iloc[:-1]
-            products = ['KH60','KH61','KL33','KK91','KK37','KK95','KK97','KK98','KL28','KL29','KL30','KL31','KL32','KL34','KK38','KK36']
+            products = ['KH60', 'KH61', 'KL33', 'KK91', 'KK37', 'KK95', 'KK97', 'KK98', 'KL28', 'KL29', 'KL30', 'KL31', 'KL32', 'KL34', 'KK38', 'KK36']
 
-
-	    
-            # filter df by product list
+            # Filter df by product list
             fbp_df = df[df['Product/Service'].isin(products)]
-            
+
             st.dataframe(fbp_df)
-	    
+
             today = date.today()
-            
-            # Convert to new excel file
-            # output_text = fbp_df.to_excel(f'Invoices filtered by product list Jan23-{today}.xlsx', index=None)
-            
+
             # Convert to a new Excel file
             output_buffer = BytesIO()
             fbp_df.to_excel(output_buffer, index=None, engine='openpyxl')
@@ -40,12 +35,12 @@ def main():
             # Create a download link
             st.markdown(get_download_link(output_text), unsafe_allow_html=True)
         except Exception as e:
-            st.write('An Error Occured.')
+            st.write('An Error Occurred.')
             st.error(f"Error reading the file: {e}")
 
 def get_download_link(text):
     today = date.today()
-    # Generate a download link for the text file
+    # Generate a download link for the Excel file
     href = f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=utf-8,{text}" download="WCN_Invoices_{today}.xlsx">Download filtered invoices as Excel file.</a>'
     return href
 
